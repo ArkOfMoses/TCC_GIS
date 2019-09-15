5b01ccf6d4b0da943df60261783431707dbfd6ab
Moises Siqueira de Oliveira <siqueiramoises14@gmail.com>
2019-09-15 12:00:38 -0300

    cpf e cnpj validados

--- primeiroCadastroMaster/cadastroDeInst/cod_cadastroInst.php
+++ primeiroCadastroMaster/cadastroDeInst/cod_cadastroInst.php
@@ -4,9 +4,9 @@
  require_once '../../bd/conexao.php';
 
     $arrayPost = [
-        "nomeFant" => FILTER_SANITIZE_FULL_SPECIAL_CHARS,
-        "razaoSoci" => FILTER_SANITIZE_FULL_SPECIAL_CHARS,
-        "cnpj" => FILTER_SANITIZE_FULL_SPECIAL_CHARS
+        "nomeFant" => FILTER_SANITIZE_SPECIAL_CHARS,
+        "razaoSoci" => FILTER_SANITIZE_SPECIAL_CHARS,
+        "cnpj" => FILTER_DEFAULT
     ];
 
     $infoPost = filter_input_array(INPUT_POST, $arrayPost);
@@ -14,11 +14,13 @@
     if($infoPost){
         $nomeFant = $infoPost['nomeFant'];
         $razaoSoci = $infoPost['razaoSoci'];
-        $cnpj = $infoPost['cnpj'];
+        $cnpj = validaCNPJ($infoPost['cnpj']);
 
         if(in_array("", $infoPost)){
             echo "<p>É necessário preencher todos os campos!</p>";
-        }else  /*  if(validaCNPJ($cnpj))  */ {
+        }else if($cnpj === false) {
+            echo "<p>CNPJ inválido!</p>";
+        }else{
             if(adicionar_inst($nomeFant, $razaoSoci, $cnpj, $pdo)){
                 echo "<script type='text/javascript'> window.location.href='../cadastroDeUnid/cadastroDeUnid.php';</script>";
             }else{
