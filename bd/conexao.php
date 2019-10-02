27ba0b8cd72d7e7bee664b142820a67fe13cd50d
Moises Siqueira de Oliveira <siqueiramoises14@gmail.com>
2019-10-02 13:40:32 -0300

    delete commit

--- bd/conexao.php
+++
@@ -1,3 +0,0 @@
-<?php 
-$pdo = new PDO("mysql:host=localhost;port=3306;dbname=bdgis", "root", "");
-$pdo->exec("set names utf8"); 
--- primeiroCadastroDiretor/funcoes/conexao.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-	$pdo = new PDO("mysql:host=localhost;port=3306;dbname=tcc", "root", ""); 
-  	$pdo->exec("set names utf8"); 
-?>
